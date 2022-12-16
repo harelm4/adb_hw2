515abe8443c348a6fd683423df8e976c6a04a2d3
hmoshayof <harel.moshayof@intel.com>
2022-12-16 16:30:04 +0200

    6/10

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,8 +1,12 @@
+import Entity.AdministratorsEntity;
+import Entity.HistoryEntity;
 import Entity.MediaItemsEntity;
 import Entity.UsersEntity;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.jboss.util.NotImplementedException;
 
 import java.sql.Date;
 import java.time.LocalDateTime;
@@ -12,8 +16,10 @@ public class Assignment {
     public static boolean isExistUsername (String username){
         Session session= hib.HibernateUtil.currentSession();
         try {
-            List<UsersEntity> items =
-                    session.createQuery("select u from UsersEntity u where u.username=username").list();
+            Query query=
+                    session.createQuery("select u from UsersEntity u where u.username=:username");
+            query.setParameter("username",username);
+            List<UsersEntity> items=query.list();
             return items.size()>0;
         }
         catch (HibernateException e){
@@ -69,4 +75,97 @@ public class Assignment {
             hib.HibernateUtil.closeSession();
         }
     }
+    public static String validateUser (String username, String password){
+        Session session= hib.HibernateUtil.currentSession();
+        try {
+            String q="select u" +
+                    " from UsersEntity u " +
+                    "where u.username=:username and u.password=:password";
+            Query query= session.createQuery(q);
+            query.setParameter("username",username);
+            query.setParameter("password",password);
+            List<UsersEntity> res=query.list();
+            if(res.size()>0){
+                return ((UsersEntity)res.get(0)).getFirstName()+' '+((UsersEntity)res.get(0)).getLastName();
+            }
+            else{
+                return "Not Found! Could be a hacker.";
+            }
+
+        }
+        catch (HibernateException e){
+            System.out.println(e);
+            return null;
+        }
+        finally {
+            hib.HibernateUtil.closeSession();
+        }
+    }
+    public static String validateAdministrator (String username,String password){
+        Session session= hib.HibernateUtil.currentSession();
+        try {
+            String q="select au" +
+                    " from AdministratorsEntity au " +
+                    "where au.username=:username and au.password=:password";
+            Query query= session.createQuery(q);
+            query.setParameter("username",username);
+            query.setParameter("password",password);
+            List<AdministratorsEntity> res=query.list();
+            if(res.size()>0){
+                return Long.toString(((AdministratorsEntity)res.get(0)).getAdminid());
+            }
+            else{
+                return "Not Found! Could be a hacker.";
+            }
+
+        }
+        catch (HibernateException e){
+            System.out.println(e);
+            return null;
+        }
+        finally {
+            hib.HibernateUtil.closeSession();
+        }
+    }
+    public static void insertToHistory (String userid, String mid){
+        Session session= hib.HibernateUtil.currentSession();
+        try {
+            Transaction tx = session.beginTransaction();
+
+            HistoryEntity history = new HistoryEntity();
+            history.setViewtime(new Date(System.currentTimeMillis()));
+            history.setUserid(Long.valueOf(userid));
+            history.setMid(Long.valueOf(mid));
+            session.save(history);
+            tx.commit();
+            System.out.println("Good job,The insertion to history table was successful!");
+        }
+        catch (HibernateException e){
+            System.out.println(e.getMessage());
+        }
+        catch (NumberFormatException e){
+            System.out.println("inputs should be numbers");
+            System.out.println(e);
+        }
+        finally {
+            hib.HibernateUtil.closeSession();
+        }
+    }
+    public static List getHistory (String userid){
+        throw new NotImplementedException();
+    }
+    public static void insertToLog (String userid){
+        throw new NotImplementedException();
+    }
+    public static int getNumberOfRegistredUsers(int n){
+        throw new NotImplementedException();
+    }
+    public static List getUsers (){
+        throw new NotImplementedException();
+    }
+    public static UsersEntity getUser (String userid){
+        throw new NotImplementedException();
+    }
+
+
 }
--- src/main.java
+++ src/main.java
@@ -7,9 +7,12 @@ import java.util.List;
 
 public class main {
     public static void main(String[] args) {
+//        boolean res=Assignment.isExistUsername("haarel");
 //        String res=Assignment.insertUser("aa","11","aaa"
 //                ,"aas","10","9","1995");
-        List res=Assignment.getTopNItems(4);
-        System.out.println(res);
+//        List res=Assignment.getTopNItems(4);
+//        String res=Assignment.validateUser("harel","1234");
+        Assignment.insertToHistory("aa","1");
+//        System.out.println(res);
     }
 }
